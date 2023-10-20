c2a55b0f7807adfc8b32dc32b4afcd954bad73fc
Rodrigo Parente <digus.parente@gmail.com>
2023-10-20 09:59:55 -0300

    added variables to configure datasets sizes

--- main.py
+++ main.py
@@ -10,17 +10,23 @@ from steps import active_learning_labelling
 from steps import super_learning_labelling
 
 
+INITIAL_DATASET_SIZE = 20
+TEST_DATASET_SIZE = 60
+ACTIVE_LEARNING_DATASET_SIZE = 100
+RANDOM_SELECTED_DATASET_SIZE = 100
+
+
 if __name__ == '__main__':
 
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    results_path = os.path.join(base_dir, 'AL_ONLINE_LABELLING_RESULTS', '')
+    results_path = os.path.join(base_dir, 'results', '')
 
     if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
         # if the application is run as a bundle, the PyInstaller
         # sets the application path into variable called _MEIPASS
         base_dir = sys._MEIPASS
         results_path = os.path.join(
-            os.path.dirname(sys.executable), 'AL_ONLINE_LABELLING_RESULTS', '')
+            os.path.dirname(sys.executable), 'results', '')
 
     os.environ['base_dir'] = base_dir
     os.environ['results_path'] = results_path
@@ -28,7 +34,8 @@ if __name__ == '__main__':
     name = get_specialist_name()
 
     print('\n - Creating pool, initial, and test datasets...')
-    creating_initial_pool_test(initial_size=20, test_size=60)
+    creating_initial_pool_test(
+        initial_size=INITIAL_DATASET_SIZE, test_size=TEST_DATASET_SIZE)
 
     print(' - Starting labelling process of initial and test datasets...')
     initial_test_labelling(name)
@@ -37,11 +44,11 @@ if __name__ == '__main__':
     active_learning_labelling(name,
                               model_name='gb',
                               query_strategy='uncertainty-sampling',
-                              number_queries=100)
+                              number_queries=ACTIVE_LEARNING_DATASET_SIZE)
 
     print(' - Starting supervised learning labelling...')
     super_learning_labelling(name,
                              model_name='gb',
-                             number_queries=100)
+                             number_queries=RANDOM_SELECTED_DATASET_SIZE)
 
     input('\nLabelling process finished! Press any key to exit.')
